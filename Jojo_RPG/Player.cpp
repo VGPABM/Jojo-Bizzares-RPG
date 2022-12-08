7b4d0f553fb36db3af4b6bd398f71e1b592e5b6b
Vincentius Mahendra <vincentiusgt23@gmail.com>
2022-12-08 17:36:39 +0700

    Feat fixes for sprite and smoother animation

--- Jojo_RPG/Player.cpp
+++ Jojo_RPG/Player.cpp
@@ -21,8 +21,8 @@ Player::Player(float x, float y, sf::Texture& texture_sheet)
 	this->createMovementComponent(500.f, 15.f, 6.f);
 	this->createAnimationComponent(texture_sheet);
 
-	this->animationComponent->addAnimation("IDLE_LEFT", 75.f, 0, 0, 5, 0, 138, 224);
-	this->animationComponent->addAnimation("WALK_LEFT", 75.f, 0, 1, 7, 1, 147, 224);
+	this->animationComponent->addAnimation("IDLE_LEFT", 75.f, 0, 0, 5, 0, 146, 212);
+	this->animationComponent->addAnimation("WALK_LEFT", 75.f, 0, 1, 7, 1, 146, 212);
 }
 
 Player::~Player()
